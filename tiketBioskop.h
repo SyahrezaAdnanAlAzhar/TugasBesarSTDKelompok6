fb31813241d3f13250b520a9728327add7252702
Syahreza Adnan Al Azhar <139461771+SyahrezaAdnanAlAzhar@users.noreply.github.com>
2023-12-13 16:34:06 +0700

    Update tiketBioskop.h

--- tiketBioskop.h
+++ tiketBioskop.h
@@ -1,6 +1,24 @@
-#ifndef TIKETBIOSKOP_H_INCLUDED
-#define TIKETBIOSKOP_H_INCLUDED
+#include <iostream>
 
-aaaaaaaaaaaaaaaaaa
 
-#endif // TIKETBIOSKOP_H_INCLUDED
+using namespace std;
+
+struct penonton{
+    string nama;
+    string email;
+    string nomorHp;
+    int jumlahTiker;
+};
+
+typedef struct elmPenonton *adrPenonton;
+
+struct elmPenonton{
+    penonton info;
+    adrPenonton next;
+};
+
+struct film{
+    string nama;
+    int menitDurasi;
+    string jamTayang;
+};
