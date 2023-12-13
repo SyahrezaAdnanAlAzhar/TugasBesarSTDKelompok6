3eaa7b7d66390643ae1f22c6a048d3e20689058f
Syahreza Adnan Al Azhar <139461771+SyahrezaAdnanAlAzhar@users.noreply.github.com>
2023-12-13 16:45:20 +0700

    Update tiketBioskop.h

--- tiketBioskop.h
+++ tiketBioskop.h
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+// Penonton
+
 struct penonton{
     string nama;
     string email;
@@ -15,10 +17,43 @@ typedef struct elmPenonton *adrPenonton;
 struct elmPenonton{
     penonton info;
     adrPenonton next;
+    listTiket tiket;
+
 };
+
+struct listPenonton{
+    adrPenonton first;
+};
+
+//film
 
 struct film{
     string nama;
     int menitDurasi;
     string jamTayang;
+};
+
+typedef struct elmFilm *adrFilm;
+
+struct elmFilm{
+    film info;
+    adrFilm next;
+};
+
+struct listFilm{
+    adrFilm first;
+};
+
+// Tiket
+
+typedef struct elmTiket *adrTiket
+
+struct elmTiket{
+    string noKursi;
+    adrFilm nextFilm;
+    adrTiket next;
+};
+
+struct listTiket{
+    adrTiket first;
 };
